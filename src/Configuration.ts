165d1634cabc18c49e09cc4dd3fb0617a8295e57
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-04 23:09:35 +0200

    feat: export AuthCallbacks

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -2,23 +2,36 @@ import { FirestoreRolesConfiguration } from "firestore-roles";
 import ow from "ow";
 
 import { Account } from "./Account";
+import { ow_catch } from "./util";
 
 export interface Configuration {
     roles: FirestoreRolesConfiguration;
-    callbacks: {
-        onAuthenticated: (account: Account) => void;
-        onError: (msg: string) => void;
-        onNotAuthenticated: () => void;
-    };
+    callbacks: Configuration.AuthCallbacks;
 }
 
 export namespace Configuration {
     export function validate(c: Configuration) {
         FirestoreRolesConfiguration.validate(c.roles, "Configuration.roles ");
 
-        ow(c.callbacks, "Configuration.callbacks", ow.object);
-        ow(c.callbacks.onAuthenticated, "Configuration.callbacks.onAuthenticated", ow.function);
-        ow(c.callbacks.onError, "Configuration.callbacks.onError", ow.function);
-        ow(c.callbacks.onNotAuthenticated, "Configuration.callbacks.onNotAuthenticated", ow.function);
+        ow(
+            c.callbacks,
+            "Configuration.callbacks",
+            ow.object.is(v => ow_catch(() => AuthCallbacks.validate(v as AuthCallbacks))),
+        );
+    }
+
+    export interface AuthCallbacks {
+        onAuthenticated: (account: Account) => void;
+        onError: (msg: string) => void;
+        onNotAuthenticated: () => void;
+    }
+
+    // tslint:disable no-shadowed-variable
+    export namespace AuthCallbacks {
+        export function validate(ac: AuthCallbacks) {
+            ow(ac.onAuthenticated, "Callbacks.onAuthenticated", ow.function);
+            ow(ac.onError, "Callbacks.onError", ow.function);
+            ow(ac.onNotAuthenticated, "Callbacks.onNotAuthenticated", ow.function);
+        }
     }
 }
