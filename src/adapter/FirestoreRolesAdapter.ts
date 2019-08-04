b26859be44a47125483b349ede98b3181523758f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-04 16:24:39 +0200

    feat: implement FirestoreRolesAdapter

---
+++ src/adapter/FirestoreRolesAdapter.ts
@@ -0,0 +1,60 @@
+// tslint:disable member-ordering
+import FirestoreRoles, { AccountRecord } from "firestore-roles";
+import * as _ from "lodash";
+
+import { Account } from "../Account";
+import { Configuration } from "../Configuration";
+
+export class FirestoreRolesAdapter {
+    private firestoreRoles: FirestoreRoles;
+    private availableRoles: string[];
+
+    public constructor(config: Configuration, firestore: firebase.firestore.Firestore) {
+        this.firestoreRoles = new FirestoreRoles(config.roles, firestore);
+        this.availableRoles = _.keys(config.roles.roles);
+    }
+
+    public getAvailableRoles(): string[] {
+        return this.availableRoles;
+    }
+
+    public isAvailableRole(role: string) {
+        return this.availableRoles.indexOf(role) >= 0;
+    }
+
+    public async userExists(uid: string) {
+        return await this.firestoreRoles.userExists(uid);
+    }
+
+    public async registerUser(account: Account) {
+        return await this.firestoreRoles.registerUser(account);
+    }
+
+    public async getUidsInRole(role: string) {
+        return await this.firestoreRoles.getUidsInRole(role);
+    }
+
+    public async getUidsRequestingRole(role: string) {
+        return await this.firestoreRoles.getUidsRequestingRole(role);
+    }
+
+    public async getAccountRecord(uid: string): Promise<AccountRecord> {
+        return await this.firestoreRoles.getAccountRecord(uid);
+    }
+
+    public async enableRole(uid: string, role: string) {
+        return await this.firestoreRoles.enableRole(uid, role);
+    }
+
+    public async disableRole(uid: string, role: string) {
+        return await this.firestoreRoles.disableRole(uid, role);
+    }
+
+    public async requestRole(uid: string, role: string) {
+        return await this.firestoreRoles.requestRole(uid, role);
+    }
+
+    public async removeRoleRequest(uid: string, role: string) {
+        return await this.firestoreRoles.removeRoleRequest(uid, role);
+    }
+}
